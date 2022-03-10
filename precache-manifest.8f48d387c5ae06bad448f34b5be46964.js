05c9f05668b7387d3c75a6c5f379de0ecbc55ff1
Dani A <danielle.arlinghaus25@gmail.com>
2022-03-10 18:41:35 -0500

    Updates

---
+++ precache-manifest.8f48d387c5ae06bad448f34b5be46964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17954af60d572e43b0a2",
+    "url": "/hotel-booking-react-app/static/css/main.e8fcd2e7.chunk.css"
+  },
+  {
+    "revision": "17954af60d572e43b0a2",
+    "url": "/hotel-booking-react-app/static/js/main.63941074.chunk.js"
+  },
+  {
+    "revision": "656d31449c8563983062",
+    "url": "/hotel-booking-react-app/static/js/runtime~main.88b33b78.js"
+  },
+  {
+    "revision": "a68ee97f505489edb6f3",
+    "url": "/hotel-booking-react-app/static/js/2.f65867d9.chunk.js"
+  },
+  {
+    "revision": "12d0e334914b481d2fb1788a82de2884",
+    "url": "/hotel-booking-react-app/index.html"
+  }
+];
